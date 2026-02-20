f9d4be0978e5f74664497080c7b3ce25745eb2cc
AlexiksA <alexandru.aranghel@yahoo.com>
2026-02-20 18:44:56 +0200

    Added a bit of frontend

--- PwManager/server.py
+++ PwManager/server.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import PwManager as pman
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/get', methods=['GET'])
 def GetPw():
+    pman.viewPw()
     data = {'user':'gigel', 'password':'12345'}
     return jsonify(data)
 
